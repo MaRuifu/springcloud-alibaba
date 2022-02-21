58f597c5d1aba2a3ab05bec938fdb63be8ea4273
maruifu <email@maruifu.cn>
2022-02-21 10:57:08 +0800

    ceshi

--- springcloud-alibaba/api-gateway/src/main/java/cn/maruifu/GatewayApplication.java
+++ springcloud-alibaba/api-gateway/src/main/java/cn/maruifu/GatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
+    //master
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
